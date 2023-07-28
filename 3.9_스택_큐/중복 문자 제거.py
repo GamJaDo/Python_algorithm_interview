5d13aef9354319a78e4f12192ec37a9fc4d40e33
GamJaDo <kim0615157@gmail.com>
2023-07-28 23:56:47 +0900

    no message

---
+++ 3.9_스택_큐/중복 문자 제거.py
@@ -0,0 +1,33 @@
+import collections
+
+"""
+def removeDuplicateLetters(s):
+
+    for char in sorted(set(s)):
+        suffix = s[s.index(char):]
+        
+        if set(s) == set(suffix):
+            
+            return char + removeDuplicateLetters(suffix.replace(char, ''))
+
+    return ''
+"""
+
+def removeDuplicateLetters(s):
+    counter, seen, stack = collections.Counter(s), set(), []
+    #print(counter)
+    for char in s:
+        counter[char] -= 1
+        if char in stack:
+            continue
+
+        while stack and char<stack[-1] and counter[stack[-1]]>0:
+            #seen.remove(stack.pop())
+            stack.pop()
+        stack.append(char)
+        #seen.add(char)
+
+    return ''.join(stack)
+
+s = "cbacdbc"
+print(removeDuplicateLetters(s))
